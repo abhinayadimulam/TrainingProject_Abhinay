a63a75c73b2812d15ecf813a86d6cc3cae80e035
abhinayadimulam <abhinay.adimulam1997@gmail.com>
2019-04-07 20:10:17 +0530

    Added UpdateDetailsControllerServlet.java

---
+++ src/com/tadigital/ecommerce/customer/servlet/UpdateDetailsControllerServlet.java
@@ -0,0 +1,51 @@
+package com.tadigital.ecommerce.customer.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tadigital.ecommerce.customer.service.CustomerService;
+
+/**
+ * Servlet implementation class UpdateDetailsControllerServlet
+ */
+@WebServlet("/updatedetails")
+public class UpdateDetailsControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String fname = request.getParameter("firstname");
+		String lname = request.getParameter("lastname");
+		String gender = request.getParameter("optradio");
+		String address = request.getParameter("address");
+		String city = request.getParameter("city");
+		String zip = request.getParameter("zip");
+		String state = request.getParameter("state");
+		String country = request.getParameter("country");
+		String cont = request.getParameter("cont");
+		String mail = request.getParameter("mail");
+
+		CustomerService customer = new CustomerService();
+
+		boolean detailsstat = customer.updatedetails(fname, lname, gender, address, city, zip, state, country, cont,
+				mail);
+
+		if (detailsstat) {
+			request.setAttribute("detailsstat", "Yes");
+			RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
+			rd.forward(request, response);
+		} else {
+			request.setAttribute("detailsstat", "No");
+			RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+}
