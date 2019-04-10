0bb98f9189e125aef49f7b5533c497e7b7ed73e7
Abhinay_Adimulam <abhinay.adimulam1997@gmail.com>
2019-04-10 10:14:59 +0530

    Added CustomerService.java

--- src/com/tadigital/ecommerce/customer/service/CustomerService.java
+++ src/com/tadigital/ecommerce/customer/service/CustomerService.java
@@ -75,7 +75,7 @@ public class CustomerService {
 		Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("zoopermanwants@gmail.com", "YOUR_PASSWORD");
+				return new PasswordAuthentication("YOUR_MAIL", "YOUR_PASSWORD");
 			}
 		});
 		try {
@@ -138,7 +138,7 @@ public class CustomerService {
 		Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("zoopermanwants@gmail.com", "YOUR_PASSWORD");
+				return new PasswordAuthentication("YOUR_MAIL", "YOUR_PASSWORD");
 			}
 		});
 		try {
